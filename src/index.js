64c3a5556f94064ef81f1bdb3a9e1d88b956cfb8
Jolan <jolan.callens@gmail.com>
2023-01-13 16:01:42 +0100

    selectstart

--- src/index.js
+++ src/index.js
@@ -170,13 +170,16 @@ function throwBall(controller) {
     camera.getWorldDirection(playerDirection);
 
     sphere.collider.center.copy(playerCollider.end).addScaledVector(playerDirection, playerCollider.radius * 1.5);
+    console.log(sphere);
     if (controller) sphere.position.copy(controller.position)
+    if (controller) sphere.velocity.applyQuaternion(controller.quaternion);
     // throw the ball with more force if we hold the button longer, and if we move forward
 
-    const impulse = 15 + 30 * (1 - Math.exp((mouseTime - performance.now()) * 0.001));
-
-    sphere.velocity.copy(playerDirection).multiplyScalar(impulse);
-    sphere.velocity.addScaledVector(playerVelocity, 2);
+    else {
+        const impulse = 15 + 30 * (1 - Math.exp((mouseTime - performance.now()) * 0.001));
+        sphere.velocity.copy(playerDirection).multiplyScalar(impulse);
+        sphere.velocity.addScaledVector(playerVelocity, 2);
+    }
 
     sphereIdx = (sphereIdx + 1) % spheres.length;
 
