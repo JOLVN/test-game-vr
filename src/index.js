da13a4d51f16fd2a1fea12a9d6c40b85fa398781
Jolan <jolan.callens@gmail.com>
2023-01-17 17:22:47 +0100

    Setup

--- src/index.js
+++ src/index.js
@@ -186,9 +186,6 @@ function throwBall(controller) {
     const impulse = controller ? 35 : 15 + 30 * (1 - Math.exp((mouseTime - performance.now()) * 0.001))
     if (controller) {
         const vel = direction;
-        vel.y /= 3;
-        vel.x /= 3;
-        vel.z /= 3;
         cube.velocity.copy(vel).multiplyScalar(impulse)
         cube.collider.center.copy(dolly.position);
     }
