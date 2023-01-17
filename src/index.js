cf8f29f9646cf382bd6bda56e0b7df4c2c1f2467
Jolan <jolan.callens@gmail.com>
2023-01-17 15:52:14 +0100

    Setup

--- src/index.js
+++ src/index.js
@@ -437,7 +437,7 @@ function setController() {
     controller1 = renderer.xr.getController(0);
     controller2 = renderer.xr.getController(1);
 
-    // dolly.add(controller1);
+    scene.add(controller1);
     scene.add(controller2);
 
     const controllerGrip1 = renderer.xr.getControllerGrip(0);
